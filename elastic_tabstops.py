312fef4c1917dcb4142a7d50d8059c95f8e27fa0
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2011-02-28 21:30:15 -0800

    Rewritten to modify the entire block

--- elastic_tabstops.py
+++ elastic_tabstops.py
@@ -122,8 +122,7 @@ class ElasticTabstopsListener(sublime_plugin.EventListener):
     for s in view.sel():
       begin_row,_ = view.rowcol(s.begin())
       end_row,_ = view.rowcol(s.end())
-      for l in range(begin_row, end_row+1):
-        selected_rows.add(l)
+      map(selected_rows.add, range(begin_row, end_row+1))
     return selected_rows
   
   def tabs_for_row(self, view, row):
@@ -132,58 +131,54 @@ class ElasticTabstopsListener(sublime_plugin.EventListener):
       row_tabs.append(tab.start())
     return row_tabs
   
-  def find_max_cell_widths(self, view, row):
-    cell_widths = [0] * len(self.tabs_for_row(view, row))
+  def cell_widths_for_row(self, view, row):
+    tabs = [-1] + self.tabs_for_row(view, row)
+    widths = [0] * (len(tabs) - 1)
+    line = view.substr(view.line(view.text_point(row,0)))
+    for i in range(0,len(tabs)-1):
+      cell = line[tabs[i]+1:tabs[i+1]]
+      if len(cell) == len(cell.rstrip()):
+        widths[i] = len(cell)
+      else:
+        widths[i] = len(cell.rstrip())+1
+    return widths
+  
+  def find_cell_widths_for_block(self, view, row):
+    cell_widths = []
     
     #starting row and backward
     rightmost_cell = len(cell_widths) - 1
     row_iter = row
     while row_iter >= 0:
-      line = view.substr(view.line(view.text_point(row_iter,0)))
-      tabs = [-1] + self.tabs_for_row(view, row_iter)
-      if len(tabs) == 1:
+      widths = self.cell_widths_for_row(view, row_iter)
+      if len(widths) == 0:
         break
-      for i in range(0,len(tabs)-1):
-        if i > rightmost_cell:
-          break
-        cell = line[tabs[i]+1:tabs[i+1]]
-        if len(cell) == len(cell.rstrip()):
-          cell_widths[i] = max(cell_widths[i],len(cell))
-        else:
-          cell_widths[i] = max(cell_widths[i],len(cell.rstrip())+1)
+      cell_widths.insert(0, widths)
       row_iter -= 1
+    first_row = row_iter + 1
     
     #forward (not including starting row)
-    rightmost_cell = len(cell_widths) - 1
     row_iter = row
     num_rows = lines_in_buffer(view)
     while row_iter < num_rows - 1:
       row_iter += 1
-      line = view.substr(view.line(view.text_point(row_iter,0)))
-      tabs = [-1] + self.tabs_for_row(view, row_iter)
-      if len(tabs) == 1:
+      widths = self.cell_widths_for_row(view, row_iter)
+      if len(widths) == 0:
         break
-      for i in range(0,len(tabs)-1):
-        if i > rightmost_cell:
-          break
-        cell = line[tabs[i]+1:tabs[i+1]]
-        if len(cell) == len(cell.rstrip()):
-          cell_widths[i] = max(cell_widths[i],len(cell))
-        else:
-          cell_widths[i] = max(cell_widths[i],len(cell.rstrip())+1)
+      cell_widths.append(widths)
     
-    return cell_widths
+    return cell_widths, first_row
   
-  def adjust_row(self, view, edit, row, start_row_tabs):
+  def adjust_row(self, view, edit, row, widths):
     row_tabs = self.tabs_for_row(view, row)
     if len(row_tabs) == 0:
       return 0
     bias = 0
-    i = 0
-    for st, it in izip(start_row_tabs,row_tabs):
-      i += 1
+    location = -1
+    for w, it in izip(widths,row_tabs):
+      location += 1 + w
       it += bias
-      difference = st - it
+      difference = location - it
       if difference == 0:
         continue
       
@@ -191,7 +186,6 @@ class ElasticTabstopsListener(sublime_plugin.EventListener):
       partial_line = view.substr(view.line(end_tab_point))[0:it]
       stripped_partial_line = partial_line.rstrip()
       ispaces = len(partial_line) - len(stripped_partial_line)
-      print(st,it,end_tab_point,ispaces,difference)
       if difference > 0:
         view.insert(edit, end_tab_point+1, (' ' * difference) + "\t")
         view.erase(edit, sublime.Region(end_tab_point, end_tab_point + 1))
@@ -199,55 +193,52 @@ class ElasticTabstopsListener(sublime_plugin.EventListener):
       if difference < 0 and ispaces >= -difference:
         view.erase(edit, sublime.Region(end_tab_point, end_tab_point + difference))
         bias += difference
-    return i
+  
+  def set_block_cell_widths_to_max(self, cell_widths):
+    starting_new_block = True
+    for c, column in enumerate(izip_longest(*cell_widths, fillvalue=None)):
+      column += (None,)
+      done = False
+      for r, width in enumerate(column):
+        if starting_new_block:
+          block_start_row = r
+          starting_new_block = False
+          max_width = 0
+        if width == None:
+          #block ended
+          block_end_row = r
+          for j in range(block_start_row, block_end_row):
+            cell_widths[j][c] = max_width
+          starting_new_block = True
+        max_width = max(max_width, width)
   
   def on_modified(self, view):
     if self.pending:
       return
     
-    selected_rows =  (self.selected_rows_by_view[view.id()] |
-                      self.get_selected_rows(view))
+    selected_rows = (self.selected_rows_by_view[view.id()] |
+                     self.get_selected_rows(view))
     try:
       self.pending = 1
       edit = view.begin_edit()
-      checked_rows = []
+      checked_rows = set()
       for row in selected_rows:
-        start_row_tabs = self.tabs_for_row(view, row)
-        max_cell_widths = self.find_max_cell_widths(view, row)
-        print("cw",max_cell_widths)
-        tab_locations = [0] * len(max_cell_widths)
-        for i,w in enumerate(max_cell_widths):
-          if i == 0:
-            tab_locations[i] = w
-          else:
-            tab_locations[i] = tab_locations[i - 1] + w + 1
-        self.adjust_row(view, edit, row, tab_locations)
+        if row in checked_rows:
+          continue
         
-        start_row_tabs = self.tabs_for_row(view, row)
-        row_iter = row
-        while row_iter > 0:
-          row_iter -= 1
-          num_tabs = self.adjust_row(view, edit, row_iter, start_row_tabs)
-          if num_tabs == 0:
-            break
-          print('r',row_iter)
-          start_row_tabs = start_row_tabs[0:num_tabs]
-        
-        start_row_tabs = self.tabs_for_row(view, row)
-        row_iter = row
-        num_rows = lines_in_buffer(view)
-        while row_iter < num_rows - 1:
-          row_iter += 1
-          num_tabs = self.adjust_row(view, edit, row_iter, start_row_tabs)
-          if num_tabs == 0:
-            break
-          print('r',row_iter)
-          start_row_tabs = start_row_tabs[0:num_tabs]
+        cell_widths_by_row, row_index = self.find_cell_widths_for_block(view, row)
+        self.set_block_cell_widths_to_max(cell_widths_by_row)
+        for widths in cell_widths_by_row:
+          checked_rows.add(row_index)
+          self.adjust_row(view, edit, row_index, widths)
+          row_index += 1
     finally:
       view.end_edit(edit)
       self.pending = 0
   
   def on_selection_modified(self, view):
+    self.selected_rows_by_view[view.id()] = self.get_selected_rows(view)
+  def on_activated(self, view):
     self.selected_rows_by_view[view.id()] = self.get_selected_rows(view)
 
 class UndoSkipOnModifiedCommand(sublime_plugin.TextCommand):
